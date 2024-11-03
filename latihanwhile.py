1dd44a7eaa056acea99f634a16de0c77452d24f3
thania8 <zerlinanathania3@gmail.com>
2024-11-03 19:25:51 +0700

    tugas DDPP_Nathania Belva Zerlina-0110224048_TI12

---
+++ latihanwhile.py
@@ -0,0 +1,50 @@
+#a = 1
+#while a<=5:
+#    print(a)
+#    a += 1
+
+#a = 1
+#while a<=10:
+#    print(a,"thania")
+#    a += 1
+
+#a = 1
+#b = "thania"
+#while a<=10:
+#    print(a,b)
+#    a += 1
+
+#n = 1
+#while n<=19:
+#    print(n)
+#    n += 2
+
+#print("=== WHILE BERINGKAT===")
+#j=1
+#while(j<=10):
+#    k=1
+#    while(k<=10):
+#        print(j*k,end="")
+#        k+=1
+#    print()
+#    j+=1
+
+#print("===FOR===")
+
+#for i in range(1,20,2):
+#    print(i)
+
+#for i in range(1,21):
+#    print(i,"mauri")
+
+print("===break statement===")
+#a = 1
+#while a < 6:
+#    print(a)
+#    if a == 3:
+#        break
+#    a += 1
+
+#n = int(input("masukkan baris ?"))
+#n +=1;
+#for i int
